96b9703afbfa493a8e5dbc3410cd78cbb7a2cfbb
Fraktalen <niclasfa@gmail.com>
2015-03-05 13:31:33 +0100

    server engine

---
+++ src/server/engine/ComputeEngine.java
@@ -0,0 +1,40 @@
+package server.engine;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import compute.Compute;
+import compute.Task;
+
+public class ComputeEngine implements Compute {
+
+    public ComputeEngine() {
+        super();
+    }
+
+    public <T> T executeTask(Task<T> t) {
+        return t.execute();
+    }
+
+    public static void main(String[] args) {
+        if (System.getSecurityManager() == null) {
+            System.setSecurityManager(new SecurityManager());
+        }
+        try {
+        	// Any client can obtain a ref to this object by using this name
+            String name = "Compute";
+            Compute engine = new ComputeEngine();
+            Compute stub =
+            		// second argument is tcp port listen on
+                (Compute) UnicastRemoteObject.exportObject(engine, 0);
+            Registry registry = LocateRegistry.getRegistry();
+            registry.rebind(name, stub);
+            System.out.println("ComputeEngine bound");
+        } catch (Exception e) {
+            System.err.println("ComputeEngine exception:");
+            e.printStackTrace();
+        }
+    }
+}
